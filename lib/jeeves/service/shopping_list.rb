83be736320f61958082d82801410a72062b013e4
Bradley Priest <bradleypriest@gmail.com>
2023-03-29 11:21:39 -0700

    Weather and Shopping list

--- lib/jeeves/service/shopping_list.rb
+++ lib/jeeves/service/shopping_list.rb
@@ -1,6 +1,36 @@
 class Jeeves
   class Service
     class ShoppingList
+      KEY = "shopping"
+
+      def process(action, context)
+        case action
+        when "AddItem"
+          @cache.sadd(KEY, context)
+          "Added #{context} to your shopping list"
+        when "RemoveItem"
+          @cache.srem(KEY, context)
+          "Removed #{context} to your shopping list"
+        when "ListItems"
+          items = @cache.smembers(KEY)
+          "Your shopping list is currently #{to_sentence(items)}"
+        end
+      end
+
+    private
+
+    def to_sentence(items)
+        case items.length
+        when 0
+          "empty"
+        when 1
+          items.first
+        when 2
+          items.join(" and ")
+        else
+          "#{items[0...-1].join(", ")} and #{items[-1]}"
+        end
+      end
     end
   end
 end
--- lib/jeeves/service/weather.rb
+++ lib/jeeves/service/weather.rb
@@ -4,8 +4,8 @@ class Jeeves
       def process(action, location)
         case action
         when 'CurrentWeather'
-          get_weather(*geocode(location))
-          nil # TODO
+          weather_json = get_weather(*geocode(location))
+          "The weather is #{weather_json['temperature'].round} degrees with #{weather_code(weather_json['weathercode'])}"
         end
       end
 
@@ -14,7 +14,6 @@ class Jeeves
       def geocode(location)
         existing = @cache.hget('geo', location)
         if existing
-          puts "Geocoding 1"
           existing.split(',')
         else
           data = HTTP.get("https://geocoding-api.open-meteo.com/v1/search", params: { name: location }).parse['results'][0]
@@ -32,20 +31,23 @@ class Jeeves
       def get_weather(lat, lng)
         HTTP.get("https://api.open-meteo.com/v1/forecast", params: { latitude: lat, longitude: lng, current_weather: true }).parse['current_weather']
       end
+
+      def weather_code(code)
+        case code
+        when 0 then "Clear sky"
+        when 1 then "Mainly clear"
+        when 2 then "Partly cloudy"
+        when 3 then "Overcast"
+        when 45, 48 then "Fog"
+        when 51 then "Light drizzle"
+        when 53, 55, 56, 57 then "Drizzle"
+        when 61, 80 then "Light rain"
+        when 63, 65, 66, 67, 81, 82 then "Rain"
+        when 71, 73, 75, 77, 85, 86 then "Snow"
+        when 95, 96, 99 then "Thunderstorms"
+        end
+      end
     end
   end
 end
 
-# 0 	Clear sky
-# 1, 2, 3 	Mainly clear, partly cloudy, and overcast
-# 45, 48 	Fog and depositing rime fog
-# 51, 53, 55 	Drizzle: Light, moderate, and dense intensity
-# 56, 57 	Freezing Drizzle: Light and dense intensity
-# 61, 63, 65 	Rain: Slight, moderate and heavy intensity
-# 66, 67 	Freezing Rain: Light and heavy intensity
-# 71, 73, 75 	Snow fall: Slight, moderate, and heavy intensity
-# 77 	Snow grains
-# 80, 81, 82 	Rain showers: Slight, moderate, and violent
-# 85, 86 	Snow showers slight and heavy
-# 95 * 	Thunderstorm: Slight or moderate
-# 96, 99 * 	Thunderstorm with slight and heavy hail
